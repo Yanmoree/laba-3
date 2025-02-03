439ddd11fe3547b39ddaa5b9385ce51270e3bd7a
Yanmoree <starostin2006yaroslav@gmail.com>
2025-02-03 17:46:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,215 @@
+// let a = 1;
+// let b = 2;
+// let c = 3;
+
+// let a = 1,
+// 	b = 2,
+// 	c = 3;
+
+//     let a, b, c; 
+// a = 1;
+// b = 2;
+// c = 3;
+
+// let a = 1 + 2;
+// alert(a);
+// let b = 3 - 2;
+// alert(b);
+// let c = 3 * 2;
+// alert(c);
+// let d = 4 / 2;
+// alert(d);
+// alert(10 % 3);
+// alert(10 % 2);
+
+// let a = 10;
+// alert(a ** 3);
+
+// let str = "abc";
+// let str2 = 'abc';
+// console.log(str);
+// console.log(str2); 
+
+// let str = "abc" + "def";
+// console.log(str);
+
+// let str1 = "abc";
+// let str2 = "def";
+// console.log(str1 + str2);
+
+// let str1 = "abc";
+// let str2 = "def";
+// console.log(str1 + "!!!" + str2);
+
+// let str1 = "abc";
+// let str2 = "def";
+// console.log(str1 + " " + str2);
+
+// let str = "abc";
+// console.log(str + " " + "def");
+
+// let str = "abc";
+// console.log(str + " def");
+
+// let str = "abcde";
+// console.log(str.length);
+// console.log("abcde".length);
+// console.log("ab de".length);
+// console.log("".length);
+
+// let str = `abc`;
+// console.log(str);
+
+// let str = "xxx";
+// let txt = "aaa " + str + " bbb";
+// console.log(txt);
+
+// let str = "xxx";
+// let txt = `aaa ${str} bbb`;
+// console.log(txt);
+
+// console.log(myVar);
+// let myVar = (10);
+
+// if (true {
+//     console.log("Hello");
+//   }
+
+// let num = 42;
+// num();
+
+// let obj = null;
+// console.log(obj.property);
+
+// let arr = new Array(-1);
+
+// function recursive() {
+// 	return recursive();
+// }
+// recursive();
+
+// let a = "123";
+// console.log("2" * "3");
+
+// console.log("2" * "3s");
+
+// let a = "1" + "2";
+// console.log(a);
+
+// let a = 1 + "2";
+// console.log(a);
+
+// let a = "1" + (2 + 3);
+// console.log(a);
+
+// let a = "1" + 2 + 3;
+// console.log(a);
+
+// let a = 2 + 3 + "1";
+// console.log(a);
+
+// let a = Number("2");
+// let b = Number("3");
+// console.log(a + b);
+
+// let a = +"2";
+// let b = +"3";
+// console.log(a + b);
+
+// let a = "2";
+// let b = +a; 
+
+// let a = "2";
+// let b = "3";
+// console.log(+a + +b); 
+
+// let num = parseInt("12px");
+// console.log(num);
+
+// let num = parseInt("12.5px");
+// console.log(num); 
+
+// let num = parseFloat("12.5px");
+// console.log(num);
+
+// let num = parseFloat("12px");
+// console.log(num);
+
+// let str = String(123);
+// let num1 = 1;
+// let num2 = 2;
+// console.log(String(num1) + String(num2));
+
+// alert(String(true));
+// alert(String(false)); 
+
+// alert("a" + true);
+
+// alert(Number(true));
+// alert(Number(false));
+
+// alert(true + 1); 
+// alert(true + true); 
+
+// let str = "abcde";
+// console.log(str[0]);
+// console.log(str[1]);
+// console.log(str[2]);
+
+// let str = "abcde";
+// let num = 3; 
+// console.log(str[num]);
+
+// let str = "abcde";
+// str[0] = "!";
+
+// let str = "abcde";
+// let last = str.length - 1; 
+
+// let str = "abcde";
+// let last = str.length - 1;
+// console.log(str[last]);
+
+// let str = "abcde";
+// console.log(str[str.length - 1]);
+
+// let num = 1;
+// num += 3; 
+
+// let num = 2;
+// num -= 3; 
+
+// let num = 2;
+// num *= 3; 
+
+// let num = 2;
+// num /= 3; 
+
+// let num = 0;
+// num++; 
+// console.log(num); 
+
+// let num = 0;
+// num--;
+// console.log(num);
+
+// let num = 0;
+// console.log(num++);
+// console.log(num);
+
+// let num = 0;
+// console.log(++num);
+
+// let num1 = 0;
+// let num2 = num1++;
+// console.log(num2); 
+// console.log(num1); 
+
+// let num1 = 0;
+// let num2 = ++num1; 
+// console.log(num2); 
+
+// let num = 0;
+// ++num;
+// num++;
+// console.log(num);
